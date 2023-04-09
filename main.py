14798d108f5ed161fb7f3e197713076e532af03f
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-04-08 22:48:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,23 @@
 """
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+This DocumentCloud Add-On uses pdfkit https://pdfkit.org/ to create a PDF snapshot of a page and save the result to DocumentCloud
 """
 
-from documentcloud.addon import AddOn
+from documentcloud.addon import SoftTimeOutAddOn
+import pdfkit
+import os
 
-
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+class Snapshot(SoftTimeoutAddOn):
+    """Add-On that uses pdfkit to take a snapshot and save it to DocumentCloud"""
 
     def main(self):
-        """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
-
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
-        for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
-
+        os.makedirs(os.path.dirname("./out/"), exist_ok=True)
+        os.chdir("./out/")
+        for url in self.data["sites"]:
+            pdfkit.from_url(url)
+        for current_path, folders, files in os.walk("./out/"):
+            for file_name in files:
+                basename = os.path.basename(file_name)
+                self.client.documents.upload(basename)
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    Snapshot().main()
