ac8242a9c94c6eea0182e66505a23a9728c12551
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2023-08-15 10:11:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ to create a PDF snapshot of a page and save the result to DocumentCloud
 from datetime import datetime
 from documentcloud.addon import SoftTimeOutAddOn
 import pdfkit
+import uuid
 
 class Snapshot(SoftTimeOutAddOn):
     """Add-On that uses pdfkit to take a snapshot and save it to DocumentCloud"""
@@ -13,11 +14,14 @@ class Snapshot(SoftTimeOutAddOn):
     def main(self):
         """Grabs datetime, runs pdfkit on the URL,
         and saves the document with that timestamp as the title"""
-        # Captures the current datetime to use as a title. 
-        now = datetime.now()
+        
         # For each URL given in the UI, pdfkit will capture the site as a PDF.
         for url in self.data["sites"]:
-            pdfkit.from_url(url, f"{now}.pdf")
+            # Captures the current datetime and a UUID to formulate as a title
+            now = datetime.now()
+            file_uuid = uuid.uuid4().hex
+            file_name = f"{now} {file_uuid}.pdf"
+            pdfkit.from_url(url, filename)
         # If a project ID is specified in the front end, upload to that project. 
         # Else it will just upload the captures to your documents in general. 
         if self.data.get("project_id"):
